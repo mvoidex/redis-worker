12a8bbba37660f038b1bbba4935a7f3bdc7cccae
Alexandr Ruchkin <voidex@live.com>
2012-10-11 17:08:35 +0400

    Revert "Removed TaskMonad"
    This reverts commit d0319c172a6b9842fd3cd34af82db259a70d601d.

--- src/System/Worker/Redis.hs
+++ src/System/Worker/Redis.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances, FlexibleContexts, MultiParamTypeClasses, UndecidableInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
 
 -- | Module provides functions to work asynchrously on tasks stored in redis.
 --
@@ -19,6 +19,8 @@
 --
 module System.Worker.Redis (
   -- * Types
+  TaskMonad(..),
+  MonadTask(..),
   runTask,
 
   -- * Strategies
@@ -45,19 +47,30 @@ import Data.ByteString (ByteString)
 import qualified Data.Map as M
 import Database.Redis
 
-inTask :: (MonadIO m, MonadReader Connection m) => Redis a -> m a
-inTask act = do
-  conn <- ask
-  liftIO $ runRedis conn act
+newtype TaskMonad m a = TaskMonad {
+  taskMonad :: ReaderT Connection m a }
+    deriving (Functor, Monad, MonadIO, MonadCatchIO, MonadReader Connection, MonadTrans)
 
-runTask :: (MonadIO m) => Connection -> ReaderT Connection m a -> m a
-runTask conn act = runReaderT act conn
+class (Monad m) => MonadTask m where
+  inTask :: Redis a -> m a
+
+instance (MonadIO m) => MonadTask (TaskMonad m) where
+  inTask act = do
+    conn <- ask
+    liftIO $ runRedis conn act
+
+instance (MonadError e m) => MonadError e (TaskMonad m) where
+  throwError = TaskMonad . throwError
+  catchError (TaskMonad a) h = TaskMonad $ catchError a (taskMonad . h)
+
+runTask :: (MonadIO m) => Connection -> TaskMonad m a -> m a
+runTask conn (TaskMonad act) = runReaderT act conn
 
 -- | General function to process tasks
 --
 -- Firstly, it moves all tasks from processing-list back to task-list, then starts popping tasks from task-list
 --
-processTasks :: (MonadIO m, MonadReader Connection m) => ByteString -> ByteString -> (ByteString -> M.Map ByteString ByteString -> m a) -> (ByteString -> m a) -> m b
+processTasks :: (MonadTask m) => ByteString -> ByteString -> (ByteString -> M.Map ByteString ByteString -> m a) -> (ByteString -> m a) -> m b
 processTasks tl pl process processFail = do
   reTask tl pl
   forever $ popTask tl pl 0 process processFail
@@ -75,7 +88,7 @@ pushTask :: ByteString -> ByteString -> M.Map ByteString ByteString -> Redis ()
 pushTask tl tid tdata = hmset tid (M.toList tdata) >> pushTaskId tl tid
 
 -- | Pop one task id (and push it to processing-list with auto-remove on end of callback) with block
-popTaskId :: (MonadIO m, MonadReader Connection m) => ByteString -> ByteString -> Integer -> (ByteString -> m a) -> m a
+popTaskId :: (MonadTask m) => ByteString -> ByteString -> Integer -> (ByteString -> m a) -> m a
 popTaskId tl pl timeout process = do
   (Right (Just i)) <- inTask $ brpoplpush tl pl timeout
   v <- process i
@@ -83,7 +96,7 @@ popTaskId tl pl timeout process = do
   return v
 
 -- | Pops one task like @popTaskId@, but takes one additional callback for fail on getting (hmgetall) data
-popTask :: (MonadIO m, MonadReader Connection m) => ByteString -> ByteString -> Integer -> (ByteString -> M.Map ByteString ByteString -> m a) -> (ByteString -> m a) -> m a
+popTask :: (MonadTask m) => ByteString -> ByteString -> Integer -> (ByteString -> M.Map ByteString ByteString -> m a) -> (ByteString -> m a) -> m a
 popTask tl pl timeout process processFail = popTaskId tl pl timeout process' where
   process' i = do
     v <- inTask $ hgetall i
@@ -92,7 +105,7 @@ popTask tl pl timeout process processFail = popTaskId tl pl timeout process' whe
       Right m -> process i $ M.fromList m
 
 -- | Move all tasks from processing-list back to task-list
-reTask :: (MonadIO m, MonadReader Connection m) => ByteString -> ByteString -> m ()
+reTask :: (MonadTask m) => ByteString -> ByteString -> m ()
 reTask tl pl = do
   (Right i) <- inTask $ rpoplpush pl tl
   case i of
